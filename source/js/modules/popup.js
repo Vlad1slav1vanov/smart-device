4d2e767c34bd2a64fc89bfda146458bc92b2b9bf
Vladislav <typlomi@gmail.com>
2023-03-04 02:28:59 +0600

    fixed popup height & focus

--- source/js/modules/popup.js
+++ source/js/modules/popup.js
@@ -2,30 +2,55 @@ const openPopupButton = document.querySelector(".header__open-popup-btn");
 const closePopupButton = document.querySelector(".popup__button-close");
 const popup = document.querySelector(".popup");
 const popupWrapper = document.querySelector(".popup__wrapper");
-const screenHeight = window.innerHeight
+
+const body = document.querySelector('body');
+const elementsToHide = body.querySelectorAll(':not(.popup__wrapper)');
+const focusedElementBeforePopup = document.activeElement;
+const firstElement = document.getElementById("popup-name-input");
+
+const hideAll = () => {
+  elementsToHide.forEach((element) => {
+    element.setAttribute('aria-hidden', 'true');
+    });
+
+  firstElement.focus();
+}
 
 const closePopup = () => {
   popup.classList.remove("popup--is-open");
+
   document.removeEventListener("keydown", (evt) => {
     if (evt.key === "Escape") {
       closePopup();
     }
   })
+
+  elementsToHide.forEach(function(element) {
+    element.removeAttribute('aria-hidden');
+  });
+
+  if (focusedElementBeforePopup) {
+    focusedElementBeforePopup.focus();
+  }
 }
 
 const openPopup = () => {
   popup.classList.add("popup--is-open");
+
   document.addEventListener("keydown", (evt) => {
     if (evt.key === "Escape") {
       closePopup();
     }
   })
+
   if (window.innerHeight < popupWrapper.offsetHeight) {
     popupWrapper.style.position = "absolute";
     popupWrapper.style.top = "0";
   } else {
     popupWrapper.style.position = "relative";
   }
+
+  hideAll();
 }
 
 const popupEvent = () => {
